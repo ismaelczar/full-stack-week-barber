78eb7fda23aff3eb07f7563e5ab5470f0efbae01
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-02-23 18:40:03 -0300

    feat: adicionando redirecionamento com useRouter

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -1,15 +1,26 @@
+'use client'
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { Barbershop } from '@prisma/client'
 import { Badge } from "./ui/badge";
 import { StarIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 
 interface BarbeshopProps {
   barbeshop: Barbershop
 }
 
 const BarberhopItem = ({ barbeshop }: BarbeshopProps) => {
+
+  const router = useRouter()
+
+  function handleBookingClick() {
+    router.push(`/barbershop/${barbeshop.id}`)
+  }
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="p-0">
@@ -35,7 +46,7 @@ const BarberhopItem = ({ barbeshop }: BarbeshopProps) => {
         <div className="px-3 pb-3">
           <h2 className="mt-2 font-bold overflow-hidden text-ellipsis text-nowrap">{barbeshop.name}</h2>
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbeshop.address}</p>
-          <Button className="bg-[#26272B] text-white w-full mt-2">Reservar</Button>
+          <Button className="bg-[#26272B] text-white w-full mt-2" onClick={handleBookingClick}>Reservar</Button>
         </div>
       </CardContent >
     </Card >
