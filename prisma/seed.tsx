645117f29b266620d405b9989a82731d6d18cfea
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-02-24 00:00:38 -0300

    feat: add imageUrl ao service

--- prisma/seed.tsx
+++ prisma/seed.tsx
@@ -27,7 +27,6 @@ async function seedDatabase() {
       "https://utfs.io/f/07842cfb-7b30-4fdc-accc-719618dfa1f2-17s.png",
       "https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png",
     ];
-
     // Nomes criativos para as barbearias
     const creativeNames = [
       "Barbearia Vintage",
@@ -56,9 +55,48 @@ async function seedDatabase() {
       "Avenida Clássica, 707",
     ];
 
-    // Criar barbearias com nomes e endereços fictícios
+    const services = [
+      {
+        name: "Corte de Cabelo",
+        description: "Estilo personalizado com as últimas tendências.",
+        price: 60.0,
+        imageUrl: "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
+      },
+      {
+        name: "Barba",
+        description: "Modelagem completa para destacar sua masculinidade.",
+        price: 40.0,
+        imageUrl: "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
+      },
+      {
+        name: "Pézinho",
+        description: "Acabamento perfeito para um visual renovado.",
+        price: 35.0,
+        imageUrl: "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
+      },
+      {
+        name: "Sobrancelha",
+        description: "Expressão acentuada com modelagem precisa.",
+        price: 20.0,
+        imageUrl: "https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png",
+      },
+      {
+        name: "Massagem",
+        description: "Relaxe com uma massagem revigorante.",
+        price: 50.0,
+        imageUrl: "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
+      },
+      {
+        name: "Hidratação",
+        description: "Hidratação profunda para cabelo e barba.",
+        price: 25.0,
+        imageUrl: "Fios hidratados, macios e brilhantes.",
+      },
+    ];
+
+    // Criar 10 barbearias com nomes e endereços fictícios
     const barbershops = [];
-    for (let i = 0; i < creativeNames.length; i++) {
+    for (let i = 0; i < 10; i++) {
       const name = creativeNames[i];
       const address = addresses[i];
       const imageUrl = images[i];
@@ -67,14 +105,28 @@ async function seedDatabase() {
         data: {
           name,
           address,
-          imageUrl,
+          imageUrl: imageUrl,
         },
       });
 
+      for (const service of services) {
+        await prisma.service.create({
+          data: {
+            name: service.name,
+            description: service.description,
+            price: service.price,
+            barbershop: {
+              connect: {
+                id: barbershop.id,
+              },
+            },
+            imageUrl: service.imageUrl,
+          },
+        });
+      }
+
       barbershops.push(barbershop);
     }
-
-    console.log("Barbearias criadas:", barbershops);
 
     // Fechar a conexão com o banco de dados
     await prisma.$disconnect();
