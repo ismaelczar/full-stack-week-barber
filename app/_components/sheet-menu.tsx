dc7f2f8ecb0e48ae368ca7f2eac2f8690ce55614
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-04-28 20:38:10 -0300

    fix: Aplicando correções no componente sheet e user

--- app/_components/sheet-menu.tsx
+++ app/_components/sheet-menu.tsx
@@ -1,9 +1,7 @@
 'use client'
-
-import { Calendar, Home, LogIn, UserCircle } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
-import { Button } from "./ui/button";
+
 import User from "./user";
 
 const SheetMenu = () => {
@@ -15,21 +13,7 @@ const SheetMenu = () => {
       </SheetHeader>
       <Separator />
 
-      <div className="flex flex-col py-6 px-5 gap-3">
-        <User />
-
-        <div className="flex flex-col gap-3 mt-2">
-          <Button className="text-sm justify-start flex items-center gap-2" variant="outline">
-            <Home size={16} />
-            Início
-          </Button>
-
-          <Button className="text-sm justify-start flex items-center gap-2" variant="outline">
-            <Calendar size={16} />
-            Agendamentos
-          </Button>
-        </div>
-      </div>
+      <User />
     </SheetContent>
   );
 }
--- app/_components/user.tsx
+++ app/_components/user.tsx
@@ -1,8 +1,9 @@
 import { Button } from "./ui/button"
-import { LogIn, LogOut, UserCircle } from "lucide-react"
+import { Calendar, Home, LogIn, LogOut, UserCircle } from "lucide-react"
 import { Avatar } from "./ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 
 const User = () => {
@@ -19,19 +20,37 @@ const User = () => {
   return (
     <>
       {status === 'authenticated' ?
-        (<div className="flex justify-between">
-          <div className="flex items-center gap-2">
-            <Avatar>
-              <AvatarImage src={data.user?.image ?? ''} alt="Foto de perfil" />
-            </Avatar>
+        (<div className="flex flex-col py-6 px-5 gap-3">
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src={data.user?.image ?? ''} alt="Foto de perfil" />
+              </Avatar>
 
-            <strong>{data.user?.name}</strong>
+              <strong>{data.user?.name}</strong>
+            </div>
+            <Button variant="secondary" onClick={handleSignOut} className="p-2.5">
+              <LogOut size={16} />
+            </Button>
+
           </div>
-          <Button variant="secondary" onClick={handleSignOut} className="p-2.5">
-            <LogOut size={16} />
-          </Button>
-        </div>) :
-        (<>
+          <div className="flex flex-col gap-3 mt-2">
+            <Button className="text-sm justify-start flex items-center gap-2" variant="outline" asChild>
+              <Link href="/">
+                <Home size={16} />
+                Início</Link>
+            </Button>
+
+            <Button className="text-sm justify-start flex items-center gap-2" variant="outline" asChild>
+              <Link href="/agendamentos">
+                <Calendar size={16} />
+                Agendamentos
+              </Link>
+            </Button>
+          </div>
+        </div>
+        ) :
+        (<div className="flex flex-col py-6 px-5 gap-3">
           <div className="flex items-center gap-2">
             <UserCircle className="text-[#838896]" size={40} />
             <strong>Olá. Faça sei login!</strong>
@@ -40,8 +59,7 @@ const User = () => {
             <LogIn size={16} />
             Fazer login
           </Button>
-
-        </>)
+        </div>)
       }
     </>
   );
