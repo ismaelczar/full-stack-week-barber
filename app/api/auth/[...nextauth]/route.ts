37475eb2f856e60f9234c7cc9879414df7c6ac18
ismaelczar <103972495+ismaelczar@users.noreply.github.com>
2024-04-28 21:01:42 -0300

    feat: Adicionando verificação quando o usuario não está logado

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,15 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { Adapter } from "next-auth/adapters"
 import { db } from "@/app/_lib/prisma"
 
 
+// A exportação é necessária para que possamos controlar nos outros componentes se o usuário está logado ou não. Isso nos permite fazer validações e bloqueios nos componentes.
+// Essa prática é recomendada em comparação com o uso do `useSession` nos demais componentes.
 
-const handler = NextAuth({
+
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
@@ -14,6 +17,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ]
-})
+}
+
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -2,6 +2,9 @@ import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "../_components/barbershop-info";
 import ServiceItem from "../_components/service-item";
 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
 interface BarbershopDetailsProps {
   params: {
     id?: string;
@@ -9,6 +12,7 @@ interface BarbershopDetailsProps {
 }
 
 const BarbershopDetails = async ({ params }: BarbershopDetailsProps) => {
+  const session = await getServerSession(authOptions)
 
   const barbershop = await db.barbershop.findUnique({
     where: {
@@ -31,7 +35,7 @@ const BarbershopDetails = async ({ params }: BarbershopDetailsProps) => {
       <div className="p-5 flex flex-col gap-4">
         {
           barbershop?.services.map((service) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
           ))
         }
       </div>
